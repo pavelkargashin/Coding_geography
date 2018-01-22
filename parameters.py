f72cf26bae00293436787960857ae3767fabab90
Paul <p.e.kargashin@mail.ru>
2018-01-22 23:16:10 +0300

    Change deacalrationof variables. Now it is done via parameters.py 38-41 changed for required version of openpyxl 46-56 - now this function works. But it doesnt work with russian 63  changed for required version of openpyxl
    The new file to manage routine
    
    Setting for data processing from different places

---
+++ parameters.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8-*-
+#HOME parameters
+TestDataFolder = "C:/Users/Paul/Documents/PaulWorkspace/CodingGeography/"
+workData = "InputBali.xlsx"
+
+
+# Work parameters
+# TestDataFolder = 'C:/PAUL/Science/GISofBALI/DataSource/'
+# workData = 'StatData.xlsx'
