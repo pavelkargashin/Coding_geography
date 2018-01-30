c04b797134c460d0cfa75ceeef7dbc745ccba3a4
IOWq750 <35732974+IOWq750@users.noreply.github.com>
2018-01-30 19:12:22 +0300

    Add files via upload

---
+++ Air_Laut_Referencing.py
@@ -0,0 +1,28 @@
+# coding: utf8
+import sys, arcpy, re, openpyxl
+reload(sys)
+sys.setdefaultencoding('utf8')
+arcpy.env.overwriteOutput=True
+
+arcpy.env.workspace = "D:/YandexDisk/Projects/Bali/Bali.gdb"
+input = "AirLaut"
+input_samples = arcpy.Copy_management(input, "AirLaut_Copy")
+
+
+# Переместим непривязанные точки в координаты точек с такой же локацией в атрибутах
+rows = arcpy.da.UpdateCursor(input_samples, ["SHAPE@X", "SHAPE@Y", "KoordinatL", "NameLocati"])
+for row in rows:
+    if row[2] == "99999999":
+        Location = row[3]
+        rows2 = arcpy.da.SearchCursor(input_samples, ["SHAPE@X", "SHAPE@Y", "KoordinatL", "NameLocati"])
+        for row2 in rows2:
+            if row2[2] != "99999999" and row2[3] == Location:
+                X = row2[0]
+                Y = row2[1]
+        del row2
+        del rows2
+        row[0] = X
+        row[1] = Y
+    rows.updateRow(row)
+del row
+del rows
