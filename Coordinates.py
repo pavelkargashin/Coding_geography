858f0204db2cd2c4a577b07658daeb838116ef16
pavelkargashin <p.e.kargashin@mail.ru>
2018-01-31 14:52:08 +0300

    22,33,35,57,61,62,66,69,75,79,84,85,89,94Все print сделаны комментариями 55,56 - исправлено добавление данных в список. Стало через append, было прямое указание номера элемента в списке 91,92 - исправлена работы с альтернативными координатами

--- Coordinates.py
+++ Coordinates.py
@@ -19,7 +19,7 @@ def decimating_pseudoDDD(input):
 def decimating_pseudoDMS(input):
     result0 = re.sub(r"''\.", ".", input)
     result = re.split(r"['',\.]+", result0)
-    print result
+    # print result
     if float(result[1]) < 60 or float(result[2]) < 60:
         Value = float(result[0]) + float(result[1])/60 + float(result[2]+"." +result[3])/3600
     else:
@@ -30,9 +30,9 @@ def decimating_pseudoDMS(input):
 def decimating_DMS(input):
     result0 = re.sub(r',', '.', input)
     result00 = re.sub(r"[°◦']+", 'o', result0)
-    print "result00="+ str(result00)
+    # print "result00="+ str(result00)
     result = re.split(r'o', result00)
-    print result
+    # print result
     Value = float(result[0]) + float(result[1])/60 + float(result[2])/3600
     return Value
 
@@ -47,47 +47,48 @@ def decimating_FalseDDD(input):
     return Value
 
 # Выбор вариантов обработки
-Text = ["8.41''8''.71","114.8''06.70''"]
+# Text = ["8.41''8''.71","114.8''06.70''"]
 def Conversion(Text):
     Output = []
     Output_Alternative = []
     if Text[0] == Text[1] == "99999999":
-        Output[0] = 9
-        Output[1] = 116
-        print Output
+        Output.append(float(9))
+        Output.append(float(116))
+        # print Output
     else:
         Input = str(Text[0]) + "_" + str(Text[1])
         result1 = re.findall(r"[°◦]", Input)
-        print result1
-        print len(result1)
+        # print result1
+        # print len(result1)
         if len(result1) is not 0:
             for Value in Text:
                 Output.append(decimating_DMS(Value))
-            print Output
+            # print Output
         else:
             result2 = re.findall(r"''", Input)
-            print len(result2)
+            # print len(result2)
             if len(result2) is not 0:
                 result3 = re.findall(r"\d'\d", Input)
                 if len(result3) is not 0:
                     for Value in Text:
                         Output.append(decimating_pseudoDMS(Value))
-                    print Output
+                    # print Output
                 elif decimating_pseudoDMS(Text[0]) == 0 or decimating_pseudoDMS(Text[1]) == 0:
                     for Value in Text:
                         Output.append(decimating_pseudoDDD(Value))
-                    print str(Output) + "_decimating_pseudoDDD"
+                    # print str(Output) + "_decimating_pseudoDDD"
                 else:
                     for Value in Text:
                         Output.append(decimating_pseudoDMS(Value))
                         Output_Alternative.append(decimating_pseudoDDD(Value))
-                    print str(Output) + "_decimating_pseudoDMS"
-                    print str(Output_Alternative) + "_decimating_pseudoDDD"
+                    # print str(Output) + "_decimating_pseudoDMS"
+                    # print str(Output_Alternative) + "_decimating_pseudoDDD"
             else:
                 for Value in Text:
                     Output.append(decimating_FalseDDD(Value))
-                print Output
+                # print Output
     Output[0] = Output[0]*(-1)
-    Output_Alternative[0] = Output_Alternative[0]*(-1)
+    if len(Output_Alternative)>0:
+        Output_Alternative[0] = Output_Alternative[0]*(-1)
     return Output, Output_Alternative
-print str(Conversion(Text))
+# print str(Conversion(Text))
