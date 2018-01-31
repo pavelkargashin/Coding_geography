9a992afedfc44a1af907a53ca369161aca7df730
pavelkargashin <p.e.kargashin@mail.ru>
2018-01-31 18:46:33 +0300

    26-29, 43-51 добавлены функции по разделению данных по этапу

--- databaseTools.py
+++ databaseTools.py
@@ -21,7 +21,12 @@ def select_shapefiles(filepath):
             continue
     return mylist
 
-
+# функция для составления списка feature classes в нужном dataset.
+# возвращает список feature classes
+def list_feature_classes(workspace):
+    arcpy.env.workspace = workspace
+    datalist = arcpy.ListFeatureClasses()
+    return datalist
 
 def extract_unique_values(fileToAnalize, fieldname):
     with arcpy.da.SearchCursor(fileToAnalize,[fieldname]) as cursor:
@@ -30,8 +35,17 @@ def extract_unique_values(fileToAnalize, fieldname):
 # Функция для разделения данных по году
 
 def split_data_year(shapefile, datapath, fieldname):
-    datalist = extract_unique_values(shapefile, fieldname )
+    datalist = extract_unique_values(shapefile, fieldname)
     for datavalue in datalist:
         criteria = '{}={}'.format(fieldname, str(int(datavalue)))
         arcpy.Select_analysis(shapefile,datapath+'_'+str(int(datavalue)), criteria)
 
+def split_data_stage(shapefile, datapath, fieldname):
+    datalist = extract_unique_values(shapefile, fieldname)
+    for datavalue in datalist:
+        criteria = "{}='{}'".format(fieldname, str(datavalue))
+        print criteria
+        if datavalue == '99999999':
+            arcpy.Select_analysis(shapefile, datapath + '_' + "NoStage", criteria)
+        else:
+            arcpy.Select_analysis(shapefile,datapath+'_'+str(datavalue), criteria)
