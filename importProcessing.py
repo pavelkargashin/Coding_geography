16cc0038b015b226fd201e04b0bcdca8221ea7fa
pavelkargashin <p.e.kargashin@mail.ru>
2018-01-31 18:47:51 +0300

    6, 20 - shp загружаются в отдельную папку

--- importProcessing.py
+++ importProcessing.py
@@ -3,8 +3,10 @@ import parameters
 import Funcs_excel2shp as cur_tools
 
 TestDataFolder = parameters.TestDataFolder
+OutputFolder = parameters.TestDataFolder+'/Shapefiles/'
 workData = parameters.workData
 exceldata = TestDataFolder+workData
+
 sheet_list = cur_tools.explore_workbook(exceldata)
 data2process = cur_tools.choose_sheets(sheet_list)# список названий тех листов, которые подходят для обработки
 mydict = cur_tools.form_names(data2process)#В этом словаре хранятся наименования для всех будущих шейпов
@@ -15,4 +17,4 @@ for item in data2process:
     print'Exporting data', str(myshape)
     tableDict = cur_tools.export_data_to_dictionary(exceldata,currentSheet=item, attr_names = shorten_field_names)
     testData = tableDict
-    cur_tools.create_shapefile(TestDataFolder+myshape, shorten_field_names, testData)
+    cur_tools.create_shapefile(OutputFolder+myshape, shorten_field_names, testData)
