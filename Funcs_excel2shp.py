fc4bf03283d24f6b31c22ea4bba4ca5d216d63f7
pavelkargashin <p.e.kargashin@mail.ru>
2018-02-02 16:26:12 +0300

    Изменения из-за параметров
    Вся обаботка теперь выполняется на основе параметров (parameters.py)

--- Funcs_excel2shp.py
+++ Funcs_excel2shp.py
@@ -7,28 +7,29 @@ import Coordinates
 import ogr
 import osr
 import re
+import os
 
 
-TestDataFolder = parameters.TestDataFolder
-workData = parameters.workData
-exceldata = TestDataFolder+workData
+# TestDataFolder = parameters.TestDataFolder
+# workData = parameters.workData
+# exceldata = TestDataFolder+workData
 
 
 # Функция для считывания файла ехсеl и выдачи списка листов. Список дистов записывается в файл txt
-def explore_workbook(inputexcel):
-    myExcel = openpyxl.load_workbook(inputexcel)
+def explore_workbook(inputDataFolder, excelName):
+    myExcel = openpyxl.load_workbook(inputDataFolder+excelName)
     mySheets = myExcel.sheetnames # get all sheets via list
-    myDoc = open(TestDataFolder + 'TEST.txt', 'w')
+    myDoc = open(inputDataFolder + 'TEST.txt', 'w')
     for item in mySheets:
         myDoc.write(item+'\n')
     myDoc.close()
     return mySheets
 
 
-def choose_sheets(sheet_list):
+def choose_sheets(sheet_list, keyword4search):
     my_sheet_list = []
     for item in sheet_list:
-        search_result = re.search(r'Kualitas Air', item)
+        search_result = re.search(keyword4search, item)
         if search_result != None:
             my_sheet_list.append(item)
         else:
@@ -130,45 +131,3 @@ def create_shapefile(ShapeName, column_names, inputTable):
         else:
             continue
     datasource = None
-
-
-
-
-
-# ФУНКЦИИ ПОКА НЕ ИСПОЛЬЗУЮТСЯ!!!!
-
-
-# Функция для экспорта данных в csv формат. На входе нужен файл и наименование листа.
-# Экспортируются данные, начиная со второй строки, то есть заголовки не выходят
-# Проблемы с экспортом - кодировки и т.д.!!!!!!!!!!!!!!!!!
-def export_data_to_csv(inputexcel, currentSheet):
-    list_temp = []
-    myExcel = openpyxl.load_workbook(inputexcel)
-    mySheet = myExcel[currentSheet]
-    with open(TestDataFolder+"new_csv.csv", 'wb') as mycsv:
-        fw = csv.writer(mycsv, delimiter = ',')
-        for row_val in range(2, mySheet.max_row+1):
-            for col_val in range(1,mySheet.max_column):
-                list_temp.append(mySheet.cell(column = col_val, row = row_val).value)
-            # print(list_temp)
-            fw.writerow(list_temp)
-            list_temp=[]
-    mycsv.close()
-
-
-# With csv writing
-def create_fields_list2(inputexcel, currentSheet):
-    list_temp = []
-    myExcel = openpyxl.load_workbook(inputexcel)
-    mySheet = myExcel[currentSheet]
-    row_val=1
-    with open(TestDataFolder+'attrNames.csv', 'w') as curCsv:
-        MyWriter = csv.writer(curCsv, delimiter = ',')
-
-        for col_val in range(1, mySheet.max_column+1):
-            # print (mySheet.cell(column=col_val, row=row_val).value)
-            list_temp.append(mySheet.cell(column=col_val, row=1).value)
-        MyWriter.writerow(list_temp)
-        curCsv.close()
-
-    return list_temp
