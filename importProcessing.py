f969eefd197cf5931fc067844a00cd114e743f0d
pavelkargashin <p.e.kargashin@mail.ru>
2018-02-02 16:23:34 +0300

    Изменения из-за новых параметров
    Вся обработка теперь опирается на параметры (prameters.py)

--- importProcessing.py
+++ importProcessing.py
@@ -2,19 +2,24 @@
 import parameters
 import Funcs_excel2shp as cur_tools
 
-TestDataFolder = parameters.TestDataFolder
-OutputFolder = parameters.TestDataFolder+'/Shapefiles/'
-workData = parameters.workData
-exceldata = TestDataFolder+workData
 
-sheet_list = cur_tools.explore_workbook(exceldata)
-data2process = cur_tools.choose_sheets(sheet_list)# список названий тех листов, которые подходят для обработки
+OutputFolder = parameters.TempData
+exceldataFolder = parameters.InputData
+keyword4search = parameters.keyword4search
+excelName = parameters.excelName
+
+sheet_list = cur_tools.explore_workbook(exceldataFolder, excelName)
+data2process = cur_tools.choose_sheets(sheet_list, keyword4search)# список названий тех листов, которые подходят для обработки
 mydict = cur_tools.form_names(data2process)#В этом словаре хранятся наименования для всех будущих шейпов
 for item in data2process:
-    attr_fields = cur_tools.create_fields_list(exceldata, currentSheet=item)
+    attr_fields = cur_tools.create_fields_list(exceldataFolder+excelName, currentSheet=item)
     shorten_field_names = cur_tools.shorten_field_name(attr_fields)
     myshape = str(mydict[item])+'.shp'
-    print'Exporting data', str(myshape)
-    tableDict = cur_tools.export_data_to_dictionary(exceldata,currentSheet=item, attr_names = shorten_field_names)
+    print 'Экспорт данных в ', str(myshape)
+    tableDict = cur_tools.export_data_to_dictionary(exceldataFolder+excelName, currentSheet=item, attr_names = shorten_field_names)
     testData = tableDict
     cur_tools.create_shapefile(OutputFolder+myshape, shorten_field_names, testData)
+
+
+
+
