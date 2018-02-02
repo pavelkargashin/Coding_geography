11fe3a65650a9c80631baaf83bab339fe2e98ed3
pavelkargashin <p.e.kargashin@mail.ru>
2018-02-02 16:24:39 +0300

    Изменения из-за параметров
    Вся обаботка теперь выполняется на основе параметров (parameters.py)

--- databaseProcessing.py
+++ databaseProcessing.py
@@ -4,27 +4,29 @@ import re
 
 import databaseTools
 import parameters
-fieldname = 'Year'
-fieldname_2 = 'Stage'
+fieldname = parameters.fieldname
+fieldname_2 = parameters.fieldname_2
 
-inputfolder_shp = parameters.TestDataFolder+'/Shapefiles/'
+inputfolder_shp = parameters.TempData
 print "Check data and create database if it is critical"
-envDatabase = databaseTools.set_database(parameters.PathToGIS, parameters.gisName, parameters.ThematicDataset)
+envDatabase = parameters.ProjectFolder+parameters.GISDataName+'.gdb'
+ThematicDataset = envDatabase + '/'+parameters.ThematicDatasetName
 # Импорт в БГД и разделение по году
 arcpy.RefreshCatalog(envDatabase)
+arcpy.env.overwriteOutput = True
 files2process = databaseTools.select_shapefiles(inputfolder_shp)
 for item in files2process:
     print item
     newfilename = re.sub(r'\.shp','', item)
     print newfilename
-    outputfile = envDatabase+'/'+parameters.ThematicDataset+'/'+newfilename
+    outputfile = ThematicDataset+'/'+newfilename
     databaseTools.split_data_year(inputfolder_shp+item,outputfile,fieldname)
 print 'Import 1 has been finished'
 # Разделение по этапу мониторинга
 arcpy.RefreshCatalog(envDatabase)
-fc2process = databaseTools.list_feature_classes(envDatabase+'/'+parameters.ThematicDataset)
+fc2process = databaseTools.list_feature_classes(ThematicDataset)
 for item in fc2process:
-    databaseTools.split_data_stage(envDatabase+'/'+parameters.ThematicDataset+'/'+item, envDatabase+'/'+parameters.ThematicDataset+'/'+item, fieldname_2)
+    databaseTools.split_data_stage(ThematicDataset+'/'+item, ThematicDataset+'/'+item, fieldname_2)
 
 
 print 'Fin'
