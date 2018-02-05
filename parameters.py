2461181e266796fa9d145c83ea3024fb664caf58
IOWq750 <karpach-am@yandex.ru>
2018-02-05 17:51:04 +0300

    Добавлены словари для работы с данными из разных среды

--- parameters.py
+++ parameters.py
@@ -19,7 +19,6 @@ ThematicDatasetName = 'ThematicData'
 BasemapDatasetName = 'BasemapData'
 AnalysisDatasetName = 'AnalysisData'
 
-
 #Processing constants
 excelName = 'StatData.xlsx'#название файла с тематическими данными
 keyword4search = "Kualitas Air"# используется при выборе нужных листов в excel
@@ -27,6 +26,22 @@ Danau = 'AirDanau'
 Sumur = 'AirSumur'
 Sungai = 'AirSungai'
 Laut = 'AirLaut'
+
+#Dictionaries
+#Accordance of sample type and field name in attribute table
+field_dict = {Danau: 'LakeName',
+              Sungai: 'RiverName',
+              Sumur: 'Area',
+              Laut: 'Point'}
+#Accordance of sample type and field name in attribute table of polygon feature class
+polyg_attr_name_dict = {Danau: 'Name',
+                   Sungai: 'Basin'}
+#Accordance of sample type and centroid feature class name
+centroid_dict = {Danau: TempData+'/Lake_Center.shp',
+                 Sungai: TempData+'/Basin_Center.shp'}
+#Accordance of sample type and polygon feature class name
+polyg_name_dict = {Danau: ProjectFolder+GISDataName+".gdb"+ThematicDatasetName + '/Lakes',
+                 Sungai: ProjectFolder+GISDataName+".gdb"+ThematicDatasetName + '/Basins'}
 
 fieldname = 'Year'#Параметр для разбивки данных по годам
 fieldname_2 = 'Stage'#Параметр для разбивки данных по месяцам
