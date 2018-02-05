2c59acb730cba9589819d5332462af8b8976ec55
pavelkargashin <p.e.kargashin@mail.ru>
2018-02-05 21:21:15 +0300

    есть ошибки экспорта

--- Regionalization.py
+++ Regionalization.py
@@ -41,18 +41,19 @@ def regionalisation_process(samples, Basins, tempGISFolder):
     fields2process = update_fields(samples_identity)
     text = dissolving_fields(fields2process, "MAX") # Расчет максимальных показателей
     # Process: Dissolve
-    Samples_Dissolve = arcpy.Dissolve_management(samples_identity, str(samples) + "_Dissolve", "Basin_1", text,
+    Samples_Dissolve = arcpy.Dissolve_management(samples_identity, str(samples) + "_Dissolve", "Basin", text,
                                                  "MULTI_PART", "DISSOLVE_LINES")
 
     # Полигональный класс бассейнов
     arcpy.SpatialJoin_analysis(Basins, Samples_Dissolve, "Basins_Samples", "JOIN_ONE_TO_ONE", "KEEP_ALL")
     # Создание серии карт по тематическим показателям
     fields = [f.name for f in arcpy.ListFields("Basins_Samples")]
-    mxd = arcpy.mapping.MapDocument(str(parameters.ProjectFolder) + "/Bali_scripting.mxd")
+    mxd = arcpy.mapping.MapDocument(str(parameters.ProjectFolder) + "Bali_scripting1.mxd")
+    print mxd.filePath, mxd.title
     df = arcpy.mapping.ListDataFrames(mxd)[0]
     lyr = arcpy.mapping.ListLayers(mxd, "Basins_Samples_2", df)[0]
     lyrFile = arcpy.mapping.Layer("Basins_Samples.lyr")
-    for field in fields[13:]:
+    for field in fields[13:-2]:
         print field
         arcpy.mapping.UpdateLayer(df, lyr, lyrFile, True)
         lyr.symbology.reclassify()
@@ -67,11 +68,11 @@ def regionalisation_process(samples, Basins, tempGISFolder):
 
 if __name__ == "__main__":
     GISFolder = parameters.ProjectFolder+parameters.GISDataName+'.gdb'
-    InputThematic = GISFolder+parameters.ThematicDatasetName
-    InputBaseMap = GISFolder+parameters.BasemapDatasetName
-    tempGISFolder = GISFolder+parameters.AnalysisDatasetName
+    InputThematic = GISFolder+'/'+parameters.ThematicDatasetName
+    InputBaseMap = GISFolder+'/'+parameters.BasemapDatasetName
+    tempGISFolder = GISFolder+'/'+parameters.AnalysisDatasetName
 
-    samples = InputThematic+"/AirDanau_2009_April"
+    samples = InputThematic+"/AirSungai_2013"
     Basin = InputBaseMap+'/Basins'
     regionalisation_process(samples, Basin, tempGISFolder)
 
