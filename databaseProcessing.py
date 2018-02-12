00a21fa7b6d3389261a544dd3efe77e81171bc30
pavelkargashin <p.e.kargashin@mail.ru>
2018-02-12 21:07:24 +0300

    Все выведено в функцию main

--- databaseProcessing.py
+++ databaseProcessing.py
@@ -1,33 +1,26 @@
 # -*- coding:utf-8-*-
 import arcpy
 import re
-
 import databaseTools
-import parameters
-fieldname = parameters.fieldname
-fieldname_2 = parameters.fieldname_2
-
-inputfolder_shp = parameters.TempData
-print "Check data and create database if it is critical"
-envDatabase = parameters.ProjectFolder+parameters.GISDataName+'.gdb'
-ThematicDataset = envDatabase + '/'+parameters.ThematicDatasetName
-# Импорт в БГД и разделение по году
-arcpy.RefreshCatalog(envDatabase)
-arcpy.env.overwriteOutput = True
-files2process = databaseTools.select_shapefiles(inputfolder_shp)
-for item in files2process:
-    print item
-    newfilename = re.sub(r'\.shp','', item)
-    print newfilename
-    outputfile = ThematicDataset+'/'+newfilename
-    databaseTools.split_data_year(inputfolder_shp+item,outputfile,fieldname)
-print 'Import 1 has been finished'
-# Разделение по этапу мониторинга
-arcpy.RefreshCatalog(envDatabase)
-fc2process = databaseTools.list_feature_classes(ThematicDataset)
-for item in fc2process:
-    databaseTools.split_data_stage(ThematicDataset+'/'+item, ThematicDataset+'/'+item, fieldname_2)
 
 
-print 'Fin'
+def main(inputfolder_shp, envDatabase, ThematicDataset, fieldname, fieldname_2):
 
+    # Импорт в БГД и разделение по году
+    arcpy.RefreshCatalog(envDatabase)
+    arcpy.env.overwriteOutput = True
+    files2process = databaseTools.select_shapefiles(inputfolder_shp)
+    for item in files2process:
+        newfilename = re.sub(r'\.shp','', item)
+        outputfile = ThematicDataset+'/'+newfilename
+        databaseTools.split_data_year(inputfolder_shp+item,outputfile,fieldname)
+    print '\n#####################\nImport Stage 1 has been finished\n#####################\n'
+    # Разделение по этапу мониторинга
+    arcpy.RefreshCatalog(envDatabase)
+    fc2process = databaseTools.list_feature_classes(ThematicDataset)
+    for item in fc2process:
+        databaseTools.split_data_stage(ThematicDataset+'/'+item, ThematicDataset+'/'+item, fieldname_2)
+    print '\n#####################\nFin\n#####################\n\nAll data is in GIS!\nNow you can work with it!!!'
+
+if __name__ == "__main__":
+    main()
