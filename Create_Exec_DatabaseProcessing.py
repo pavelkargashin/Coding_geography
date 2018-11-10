468cb2207623b20928b7121d540087301894fe81
pavelkargashin <p.e.kargashin@mail.ru>
2018-11-10 16:05:29 +0300

    Исправлено под новую структуру.

--- Create_Exec_DatabaseProcessing.py
+++ Create_Exec_DatabaseProcessing.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8-*-
 import arcpy
 import re
-import databaseTools
+import Create_Tools_MakeShapefiles
 
 
 def main(inputfolder_shp, envDatabase, ThematicDataset, fieldname, fieldname_2):
@@ -9,17 +9,17 @@ def main(inputfolder_shp, envDatabase, ThematicDataset, fieldname, fieldname_2):
     # Импорт в БГД и разделение по году
     arcpy.RefreshCatalog(envDatabase)
     arcpy.env.overwriteOutput = True
-    files2process = databaseTools.select_shapefiles(inputfolder_shp)
+    files2process = Create_Tools_MakeShapefiles.select_shapefiles(inputfolder_shp)
     for item in files2process:
         newfilename = re.sub(r'\.shp','', item)
         outputfile = ThematicDataset+'/'+newfilename
-        databaseTools.split_data_year(inputfolder_shp+item,outputfile,fieldname)
+        Create_Tools_MakeShapefiles.split_data_year(inputfolder_shp + item, outputfile, fieldname)
     print '\n#####################\nImport Stage 1 has been finished\n#####################\n'
     # Разделение по этапу мониторинга
     arcpy.RefreshCatalog(envDatabase)
-    fc2process = databaseTools.list_feature_classes(ThematicDataset)
+    fc2process = Create_Tools_MakeShapefiles.list_feature_classes(ThematicDataset)
     for item in fc2process:
-        databaseTools.split_data_stage(ThematicDataset+'/'+item, ThematicDataset+'/'+item, fieldname_2)
+        Create_Tools_MakeShapefiles.split_data_stage(ThematicDataset + '/' + item, ThematicDataset + '/' + item, fieldname_2)
         # arcpy.Delete_management(ThematicDataset+'/'+item) #Пока строка не используется. до тех пор пока не будет поправлен файл Regionalization.py
 
     print '\n#####################\nFin\n#####################\n\nAll data is in GIS!\nNow you can work with it!!!'
